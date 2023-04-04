3b7866dc0ba2812aa8ca1553669771d112db67b2
GevitarGaming04 <gevitargaming04@gmail.com>
2023-04-04 17:45:58 +0800

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,5 @@
 import sys
 import subprocess
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pygame'])
 
 import pygame
